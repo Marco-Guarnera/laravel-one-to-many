e0c18d2e536e4f1ec513be475f8e3f30f22f34c6
Marco-Guarnera <Darkaquon93@gmail.com>
2024-11-08 09:26:02 +0100

    Updated ProjectSeeder.php in seeders

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -12,8 +13,11 @@ class ProjectSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(Faker $faker) : void {
+        $types_list = Type::all()->pluck('id');
+
         for ($i = 0; $i < 25; $i++) {
             $project = new Project();
+            $project->type_id = $faker->randomElement($types_list);
             $project->name = $faker->unique()->word();
             $project->description = $faker->text(250);
             $project->save();
